48a8afaeb61c511cd722c4729cfb2e6c7b2d2361
Manuel Ignacio Manríquez <manuel.manriquez.b@usach.cl>
2019-09-15 00:55:55 -0300

    Terminado TDA de personajes, lo que significa todo hasta modificadores. Solo queda implementar las otras funciones.

--- TDA_personajes.rkt
+++ TDA_personajes.rkt
@@ -3,14 +3,16 @@
 ;;del laboratorio de paradigmas de programación. En particular, contendrá el TDA de
 ;;los personajes del equipo.
 
+;;Representacion: personaje -> '(entero_posicionX entero_posicionY entero_vida)
+
 
 ;;Funcion constructora de personajes
 ;;Dominio: Entero X Entero X Entero
 ;;Recorrido: personaje (entero)
 (define (personaje X Y vida) (
-                              if (and (> X 0)
-                                      (> Y 0)
-                                      (> vida 0)
+                              if (and (> X -1)
+                                      (> Y -1)
+                                      (> vida -1)
                                       )
                                  (list X Y vida)
                                  0
@@ -22,12 +24,76 @@
 ;; Recorrido: booleano
 (define (personaje? p) (
                         if(and
-                          (> (car p) 0)
-                          (> (car (cdr p)) 0)
-                          (> (car (cdr (cdr p))) 0)
+                          (> (car p) -1)
+                          (> (car (cdr p)) -1)
+                          (> (car (cdr (cdr p))) -1)
                           )
                           #t
                           #f)
   )
 
+;; Funcion selectora de posicion X de personajes
+;; Dominio: personaje
+;; Recorrido: entero, representa posicion X
+(define (getXpersonaje p) (
+                           if (personaje? p)
+                               (car p)
+                               null
+                               )
+                           )
+ 
 
+;; Funcion selectora de posicion Y de personajes
+;; Dominio: personaje
+;; Recorrido: entero, representa posicion Y
+(define (getYpersonaje p) (
+                           if (personaje? p)
+                               (car (cdr p))
+                               null
+                               )
+                           
+  )
+
+;; Funcion selectora de vida de personajes
+;; Dominio: personaje
+;; Recorrido: entero, representa vida restante
+(define (getVidapersonaje p) (
+                              if(personaje? p)
+                                 (car (cdr (cdr p)))
+                                 null
+                                 
+                              )
+  )
+
+;; Funcion modificadora de posicion X de personaje
+;; Dominio: personaje X entero
+;; Recorrido: personaje
+(define (setXpersonaje p x) (
+                            if (and (personaje? p) (number? x) (> x -1))
+                               (personaje x (getYpersonaje p) (getVidapersonaje p))
+                               null
+                               )
+  )
+
+;; Funcion modificadora de posicion Y de personaje
+;; Dominio: personaje X entero
+;; Recorrido: personaje
+(define (setYpersonaje p y) (
+                             if(and (personaje? p) (number? y) (> y -1))
+                               (personaje (getXpersonaje p) y (getVidapersonaje p))
+                               null
+                               )
+  )
+
+;; Funcion modificadora de vida de personaje
+;; Dominio: personaje X entero
+;; Recorrido: personaje
+(define (setVidapersonaje p v) (
+                                if(and (personaje? p) (number? v) (> v -1))
+                                  (personaje (getXpersonaje p) (getYpersonaje p) v)
+                                  null
+                                  )
+  )
+
+
+  
